e7a47343b958215246134a18e9142c8cd3e68252
Rajat Keshri <keshri.r@northeastern.edu>
2023-10-12 20:52:52 -0400

    class work - Rajat Keshri

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,14 @@ import Profile from './qcomps/profile_mistake.js'
 import FirstComp from './qcomps/firstcomp.js'
 import Bios from './qcomps/bios.js';
 import TodoList from './qcomps/todos.js';
+import Avatar from './components/profile_props.js';
+import GallaryProps from './qcomps/gallery_props.js';
+
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <TodoList />
+        <GallaryProps />
     </div>
   )
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,55 +1,51 @@
+function DisplayImage({person}){
+  return (
+      <section className="profile">
+        <h2>{person.name}</h2>
+        <img
+          className="avatar"
+          src={person.url}
+          alt={person.name}
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            {person.profession}
+          </li>
+          <li>
+            <b>Awards: {person.awardnumber} </b>
+            ({person.awards})
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {person.discovered}
+          </li>
+        </ul>
+      </section>
+  );
+}
+
 export default function Gallery() {
+
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <DisplayImage person={{name:'Maria Skłodowska-Curie',
+        url: 'https://i.imgur.com/szV5sdGs.jpg',
+        profession: 'physicist and chemist',
+        awardnumber: "4",
+        awards: "Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal", 
+        discovered: "polonium (element)"}}/>
+
+
+      <DisplayImage person={{name:'Katsuko Saruhashi',
+        url: 'https://i.imgur.com/YfeOqp2s.jpg',
+        profession: 'geochemist',
+        awardnumber: "4",
+        awards: "Miyake Prize for geochemistry, Tanaka Prize", 
+        discovered: "a method for measuring carbon dioxide in seawater"}}/>
     </div>
   );
 }
